9f194e3ddf65034ae656ee32e19c0e1b8354bf86
tlmooreclemson <tlmoore1928@gmail.com>
2019-11-21 20:30:11 +0100

    Update random_recipe.py

--- random_recipe.py
+++ random_recipe.py
@@ -1,1 +1,22 @@
 import tkinter
+import random
+
+lunch = ['Tuna Melt', 'Peas and Pancetta', 'CondiRiso', 'GabiBabis Special Soup']
+dinner = ['GabiBabis Special Chicken and Potatoes', 'Gateau', 'Polpettone']
+
+choice_lunch = lunch[random.randint(1, len(lunch))]
+choice_dinner = dinner[random.randint(1,len(dinner))]
+
+# Generate the GUI window
+window = tkinter.Tk()
+
+# Rename the window
+window.title("Random Meal Decider")
+
+# Create a function for choosing dinner
+def call_dinner():
+    tkinter.Label(window, text = choice_dinner).pack()
+
+tkinter.Button(window, text = "Random Dinner", command = call_dinner).pack()
+
+window.mainloop()
